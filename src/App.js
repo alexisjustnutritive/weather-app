aac832e645b1e7c2b28c25c145979c4cd2f6a508
Just Nutritive <alexise@justnutritive.com>
2019-07-30 15:54:10 -0400

    working

--- src/App.js
+++ src/App.js
@@ -10,18 +10,33 @@ function App() {
     const [ queryWeather, setQueryWeather ] = useState( {} );
     const [weather, setWeather] = useState( {} );
 
-    const getWeatherData = async () => {
-        let apiUrl =  `https://api.openweathermap.org/data/2.5/weather?q=${ queryWeather.city },${ queryWeather.country }&appid=447a0a328f23d03a2f0f5df3918cf3a0&units=metric`;
-        let res = await fetch( apiUrl );
-        let data = await res.json();
-        setWeather( data );
-    }
-
     useEffect( () => {
         if ( Object.entries( queryWeather ).length > 0 ) {
+            const getWeatherData = async () => {
+                let apiUrl =  `https://api.openweathermap.org/data/2.5/weather?q=${ queryWeather.city },${ queryWeather.country }&appid=447a0a328f23d03a2f0f5df3918cf3a0&units=metric`;
+                let res = await fetch( apiUrl );
+                let data = await res.json();
+                setWeather( data );
+            }
+
             getWeatherData();
         }
     }, [ queryWeather ] );
+
+    useEffect(() => {
+        console.log( weather );
+        if ( Object.entries( weather ).length > 0 ) {
+            document.body.classList.forEach( classL => {
+                document.body.classList.remove( classL );
+            } );
+            
+            console.log( weather );
+            let classList = weather.weather[0].main.toLowerCase();
+            classList = classList.replace(/\s/g,'');
+            document.querySelector( '.container-page' ).classList.add( classList );
+            // document.body.classList.add( classList );
+        }
+    }, [ weather ]);
 
     let weatherElement;
     if ( Object.entries( weather ).length > 0 ) {
@@ -30,8 +45,8 @@ function App() {
         } else {
             weatherElement = ( <Weather weather={ weather } /> ) ;
         }
-    } 
-
+    }     
+    
   return (
     <Container className="bg-secondary container-page">
         <div className="header-container">
